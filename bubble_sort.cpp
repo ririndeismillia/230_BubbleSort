a0337c55cea3500c48ae1c462778a83ae2bf4335
ririndeismillia <ririndeismillia@gmail.com>
2026-03-03 11:43:03 +0700

    main

--- bubble_sort.cpp
+++ bubble_sort.cpp
@@ -58,3 +58,38 @@ void bubbleShortArray()
 
     }  while (pass <= n - 1);
 }
+
+void display()
+{
+    cout << endl;
+    cout << "==================================" << endl;
+    cout << "Element Array yang telah tersusun" << endl;
+    cout << "==================================" << endl;
+    cout << endl;
+    for (int j = 0; j < n; j++)
+    {
+        cout << arr[j];
+        if (j < n - 1)
+        {
+            cout << " --> ";
+        }
+    }
+    cout << endl;
+    cout << endl;
+
+    cout << "Jumlah pass = " << n - 1 << endl;
+    cout << endl;
+    cout << endl;
+}
+
+int main()
+{
+
+    input();
+
+    bubbleSortArray();
+    display();
+
+    system("pause");
+    return 0;
+}
